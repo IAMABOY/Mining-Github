eb84fc98a5927bfe6e8e5e7f44dbe9b10d131f5c
jiawei <shan_jiawei@126.com>
2019-10-11 13:21:37 +0800

    add more SQL commands

--- bq/github_overview.sql
+++ bq/github_overview.sql
@@ -1,52 +1,52 @@
---使用bigquery-public-data:github_repos数据集
---该数据集的跟新较为频繁，数据较新
---比之使用githubarchive中的数据集要方便
+--bq会用到三个关于Github数据集bigquery-public-data:github_repos,githubarchive,ghtorrent-bq
+--bigquery-public-data:github_repos,githubarchive更新频繁，ghtorrent-bq好久没有更新，但是可以手动导入其官网数据
+--三个数据集都有较为实用的信息，哪个方便使用哪一个
+
+
 
 --查询Github上总的仓库数量
 --使用bigquery-public-data:github_repos.languages数据表
+--3347497 repos by 2019 10/05
 
 SELECT COUNT(*)
 FROM [bigquery-public-data:github_repos.languages]
 
---3347497 repos by 2019 10/05
 
 
 --查询Github上总commits数量
 --使用bigquery-public-data:github_repos.commits数据表
+-- 235186010 commits by 2019 10/05
 
 SELECT COUNT(*)
 FROM [bigquery-public-data:github_repos.commits]
-
--- 235186010 commits by 2019 10/05
 
 
 --查询Github上总编程语言个数
 --可以使用bigquery-public-data:github_repos.languages数据表
 --注意的是结果中将Makefile,json,yaml等文件格式也标记成了编程语言
+-- 386 languages by 2019 10/05
 
 SELECT language.name,COUNT(*) counts
 FROM [bigquery-public-data:github_repos.languages]
 GROUP BY language.name
 ORDER BY counts DESC;
 
--- 386 languages by 2019 10/05
-
 
 --查询Github上license种类
 --可以使用bigquery-public-data:github_repos.licenses数据表
+-- 15 licenses by 2019 10/05
 
 SELECT license,COUNT(*) counts
 FROM [bigquery-public-data:github_repos.licenses]
 GROUP BY license
 ORDER BY counts DESC;
 
--- 15 licenses by 2019 10/05
-
 
 --查询github上有多少个独立的注册用户
+--24154883 users by 2018 04/01
 
 SELECT COUNT(*)
 FROM [ghtorrent-bq:ght_2018_04_01.users] 
 
 
---24154883 users by 2018 04/01
+--查询github上有多少个曾经提交记录的用户
--- bq/github_rank.sql
+++ bq/github_rank.sql
@@ -1,3 +1,10 @@
+--bq会用到三个关于Github数据集bigquery-public-data:github_repos,githubarchive,ghtorrent-bq
+--bigquery-public-data:github_repos,githubarchive更新频繁，ghtorrent-bq好久没有更新，但是可以手动导入其官网数据
+--三个数据集都有较为实用的信息，哪个方便使用哪一个
+
+
+
+
 --事件类型为PushEvent的payload含有email
 --中国高校2018年push数量100强
 --北大，上交，中科大位列前三甲，盛产工程师的北邮，华科也在前10之列
@@ -17,10 +24,10 @@ LIMIT 100;
 
 
 --查看具有国家信息的用户占比
+--0.07504958728220708 repos by 2018 04/01
+
 SELECT SUM(country_code!='\\N')/COUNT(*)
 FROM [ghtorrent-bq:ght_2018_04_01.users]
-
---0.07504958728220708 repos by 2018 04/01
 
 
 --ghtorrent-bq数据集users表记录了注册者的国家信息，有的用户没有
@@ -55,10 +62,24 @@ ORDER BY pushes DESC
 LIMIT 100
 
 
+--2019年以来被fork次数最多的项目，一定程度上代表了该项目的流行程度
+--996.ICU赫然在列，我们中国的程序员应该很熟悉
+--当然还有tensorflow，opencv这种这几年比较流行的项目
+SELECT table2019.repo.name AS repo_name,COUNT(DISTINCT table2019.actor.id) AS fork_counts
+FROM 
+  (
+  SELECT *
+  FROM TABLE_QUERY([githubarchive:month],'table_id CONTAINS "2019"')
+  ) AS table2019
+  WHERE table2019.type = 'ForkEvent'
+GROUP BY repo_name
+ORDER BY fork_counts DESC
+LIMIT 20
 
 
 
 --哪个项目被提交的次数最多
+
 SELECT repo_name, COUNT(*) AS commit_num
 FROM [bigquery-public-data:github_repos.commits] 
 GROUP BY repo_name
@@ -66,6 +87,7 @@ ORDER BY commit_num DESC
 
 
 --2019/01/01年以来提交最频繁的项目
+
 SELECT repo_name, COUNT(*) AS commit_num
 FROM [bigquery-public-data:github_repos.commits] 
 WHERE committer.date.seconds >= 1546272000
@@ -73,20 +95,22 @@ GROUP BY repo_name
 ORDER BY commit_num DESC
 
 
-
-
-
 --查询github修改代码次数最多的大神
+--2408004 人提交过代码 by 2019/10/05
+
 SELECT author.name,COUNT(*) AS commits
 FROM [bigquery-public-data:github_repos.commits] 
 GROUP BY author.name
 ORDER BY commits DESC
---2408004 提交过代码
+
 
 --2019/01/01以来修改代码次数用户排名
 --author 是 patch 的作者，committer 是把 patch 应用到 repository 里的人 (很多项目限制只有少数人可以 apply patch，但大家都可以把 patch 发送给这些人)
+--看看有没有你熟悉的大神,我严重怀疑提交次数过万的大概率是全职
+
 SELECT author.name,COUNT(*) AS commits
 FROM [bigquery-public-data:github_repos.commits] 
 WHERE committer.date.seconds >= 1546272000
 GROUP BY author.name
 ORDER BY commits DESC
+
